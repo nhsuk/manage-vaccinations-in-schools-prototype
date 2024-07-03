2d9a451c47be0a6e220dee581ec27667fda6b263
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-07-03 17:25:40 +0100

    Add new or edit campaign

--- app/controllers/campaign.js
+++ app/controllers/campaign.js
@@ -1,3 +1,4 @@
+import { wizard } from 'nhsuk-prototype-rig'
 import { Campaign } from '../models/campaign.js'
 import { Session } from '../models/session.js'
 import { Vaccine } from '../models/vaccine.js'
@@ -7,13 +8,9 @@ export const campaignController = {
     const { data } = request.session
 
     response.render('campaigns/list', {
-      campaigns: Object.values(data.campaigns).map((campaign) => {
-        campaign = new Campaign(campaign)
-        campaign.vaccines = campaign.vaccines.map(
-          (vaccine) => new Vaccine(data.vaccines[vaccine]).brandWithName
-        )
-        return campaign
-      })
+      campaigns: Object.values(data.campaigns).map(
+        (campaign) => new Campaign(campaign)
+      )
     })
   },
 
@@ -41,9 +38,6 @@ export const campaignController = {
         )
         return session
       })
-    request.app.locals.vaccines = campaign.vaccines.map(
-      (vaccine) => new Vaccine(data.vaccines[vaccine]).brandWithName
-    )
 
     next()
   },
@@ -58,5 +52,100 @@ export const campaignController = {
     })
 
     response.render('campaigns/edit')
+  },
+
+  new(request, response) {
+    const { data } = request.session
+
+    // Delete previous data
+    delete data.campaign
+    delete data.wizard
+
+    const campaign = new Campaign()
+
+    data.wizard = campaign
+
+    response.redirect(`/campaigns/${campaign.uuid}/new/details`)
+  },
+
+  update(request, response) {
+    const { campaign } = request.app.locals
+    const { form, uuid } = request.params
+    const { data } = request.session
+    const { __ } = response.locals
+
+    const updatedCampaign = new Campaign({
+      ...campaign, // Previous values
+      ...data.wizard, // Wizard values
+      ...(data.token && { created_user_uuid: data.token.uuid })
+    })
+
+    data.campaigns[uuid] = updatedCampaign
+
+    delete data.wizard
+
+    const action = form === 'edit' ? 'update' : 'create'
+    request.flash(
+      'success',
+      __(`campaign.success.${action}`, { campaign: updatedCampaign })
+    )
+
+    response.redirect(`/campaigns/${uuid}`)
+  },
+
+  readForm(request, response, next) {
+    const { campaign } = request.app.locals
+    const { form, uuid } = request.params
+    const { data } = request.session
+
+    request.app.locals.campaign = new Campaign({
+      ...(form === 'edit' && campaign), // Previous values
+      ...data.wizard // Wizard values,
+    })
+
+    const journey = {
+      [`/`]: {},
+      [`/${uuid}/${form}/details`]: {},
+      [`/${uuid}/${form}/period`]: {},
+      [`/${uuid}/${form}/vaccines`]: {},
+      [`/${uuid}/${form}/check-answers`]: {},
+      [`/${uuid}`]: {}
+    }
+
+    response.locals.paths = {
+      ...wizard(journey, request),
+      ...(form === 'edit' && {
+        back: `/campaigns/${uuid}/edit`,
+        next: `/campaigns/${uuid}/edit`
+      })
+    }
+
+    response.locals.vaccineItems = Object.values(data.vaccines)
+      .map((vaccine) => new Vaccine(vaccine))
+      .map((vaccine) => ({
+        text: vaccine.brandWithName,
+        value: vaccine.gtin
+      }))
+
+    next()
+  },
+
+  showForm(request, response) {
+    const { view } = request.params
+
+    response.render(`campaigns/form/${view}`)
+  },
+
+  updateForm(request, response) {
+    const { campaign } = request.app.locals
+    const { data } = request.session
+    const { paths } = response.locals
+
+    data.wizard = new Campaign({
+      ...campaign, // Previous values
+      ...request.body.campaign // New value
+    })
+
+    response.redirect(paths.next)
   }
 }
