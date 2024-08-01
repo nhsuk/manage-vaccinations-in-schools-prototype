f5d52534dfa38ad0c5fb4074aa16e599cae528c1
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-08-01 17:54:03 +0100

    Download vaccination reports

---
+++ app/routes/download.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { downloadController } from '../controllers/download.js'
+
+const router = express.Router({ strict: true, mergeParams: true })
+
+router.get('/', downloadController.redirect)
+
+router.get('/new', downloadController.new)
+router.post('/:id/?:form(new)/check-answers', downloadController.update)
+
+router.all('/:id/?:form(new|edit)/:view', downloadController.readForm)
+router.get('/:id/?:form(new|edit)/:view', downloadController.showForm)
+router.post('/:id/?:form(new|edit)/:view', downloadController.updateForm)
+
+export const downloadRoutes = router
