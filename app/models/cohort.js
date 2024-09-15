f8d01c75428f88a45ae60ff22bcaa5e6b0e6fa11
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-09-15 19:10:06 +0100

    Include programme type in cohort name

--- app/models/cohort.js
+++ app/models/cohort.js
@@ -1,4 +1,4 @@
-import { ProgrammeCycle } from './programme.js'
+import { ProgrammeCycle, ProgrammeType } from './programme.js'
 import { Record } from './record.js'
 import { formatYearGroup } from '../utils/string.js'
 
@@ -29,6 +29,7 @@ export function getRecordsFromYearGroup(records, yearGroup) {
  * @property {number} yearGroup - Year group
  * @property {Map<Record>} records - Records
  * @property {string} [programme_pid] - Programme ID
+ * @property {ProgrammeType} [programme_type] - Programme type
  * @function ns - Namespace
  * @function uri - URL
  */
@@ -39,6 +40,7 @@ export class Cohort {
     this.yearGroup = options?.yearGroup
     this.records = options?.records || []
     this.programme_pid = options?.programme_pid
+    this.programme_type = options?.programme_type
   }
 
   static generate(programme, records, yearGroup, user) {
@@ -48,7 +50,8 @@ export class Cohort {
       created_user_uid: user.uid,
       yearGroup,
       records,
-      programme_pid: programme.pid
+      programme_pid: programme.pid,
+      programme_type: programme.type
     })
   }
 
@@ -63,7 +66,9 @@ export class Cohort {
   }
 
   get name() {
-    return `${this.formatted.yearGroup} (${this.cycle})`
+    const type = ProgrammeType[this.programme_type]
+
+    return `${type} ${this.cycle} (${this.formatted.yearGroup})`
   }
 
   get year() {
