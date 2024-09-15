72441a002c1cfcea242c94e6c244d993f7464328
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-09-15 23:45:59 +0100

    Refactor record controller

--- app/controllers/record.js
+++ app/controllers/record.js
@@ -2,31 +2,41 @@ import { Record } from '../models/record.js'
 import { Vaccination } from '../models/vaccination.js'
 
 export const recordController = {
-  list(request, response) {
+  readAll(request, response, next) {
     const { data } = request.session
 
-    response.render('records/list', {
-      records: Object.values(data.records).map((record) => new Record(record))
+    const records = Object.values(data.records).map((record) => {
+      record = new Record(record)
+
+      // Add complete vaccination record
+      record.vaccinations = record.vaccinations.map(
+        (uuid) => new Vaccination(data.vaccinations[uuid])
+      )
+
+      return record
     })
+
+    response.locals.records = records
+
+    next()
   },
 
-  show(request, response) {
-    response.render('records/show')
+  showAll(request, response) {
+    response.render('records/list')
   },
 
   read(request, response, next) {
     const { nhsn } = request.params
-    const { data } = request.session
+    const { records } = response.locals
 
-    const record = new Record(data.records[nhsn])
-
-    // Add complete vaccination record
-    record.vaccinations = record.vaccinations.map(
-      (uuid) => new Vaccination(data.vaccinations[uuid])
-    )
+    const record = records.find((record) => record.nhsn === nhsn)
 
     response.locals.record = record
 
     next()
+  },
+
+  show(request, response) {
+    response.render('records/show')
   }
 }
--- app/routes/record.js
+++ app/routes/record.js
@@ -3,10 +3,10 @@ import { recordController } from '../controllers/record.js'
 
 const router = express.Router({ strict: true })
 
-router.get('/', recordController.list)
+router.all('/*', recordController.readAll)
+router.get('/', recordController.showAll)
 
 router.all('/:nhsn*', recordController.read)
-
-router.get('/:nhsn', recordController.show)
+router.get('/:nhsn/:view?', recordController.show)
 
 export const recordRoutes = router
