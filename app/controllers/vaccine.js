bd99b382626449e20d9498a1ad5526eddfb0fe3f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-09-16 01:14:26 +0100

    Refactor vaccine controller

--- app/controllers/vaccine.js
+++ app/controllers/vaccine.js
@@ -2,47 +2,55 @@ import { Batch } from '../models/batch.js'
 import { Vaccine } from '../models/vaccine.js'
 
 export const vaccineController = {
-  action(type) {
-    return (request, response) => {
-      response.render('vaccines/action', { type })
-    }
-  },
-
-  list(request, response) {
+  readAll(request, response, next) {
     const { data } = request.session
 
-    response.render('vaccines/list', {
-      vaccines: Object.values(data.vaccines).map((vaccine) => {
-        vaccine = new Vaccine(vaccine)
+    const vaccines = Object.values(data.vaccines).map((vaccine) => {
+      vaccine = new Vaccine(vaccine)
 
-        vaccine.batches = Object.values(data.batches)
-          .filter((batch) => batch.vaccine_gtin === vaccine.gtin)
-          .map((batch) => new Batch(batch))
+      vaccine.batches = Object.values(data.batches)
+        .filter((batch) => batch.vaccine_gtin === vaccine.gtin)
+        .map((batch) => new Batch(batch))
 
-        return vaccine
-      })
+      return vaccine
     })
-  },
 
-  show(request, response) {
-    response.render('vaccines/show')
-  },
-
-  read(request, response, next) {
-    const { gtin } = request.params
-    const { data } = request.session
-
-    request.app.locals.vaccine = new Vaccine(data.vaccines[gtin])
+    response.locals.vaccines = vaccines
 
     next()
   },
 
+  showAll(request, response) {
+    response.render('vaccine/list')
+  },
+
+  read(request, response, next) {
+    const { gtin } = request.params
+    const { vaccines } = response.locals
+
+    const vaccine = vaccines.find((vaccine) => vaccine.gtin === gtin)
+
+    response.locals.vaccine = vaccine
+
+    next()
+  },
+
+  show(request, response) {
+    response.render('vaccine/show')
+  },
+
+  action(type) {
+    return (request, response) => {
+      response.render('vaccine/action', { type })
+    }
+  },
+
   delete(request, response) {
-    const { vaccine } = request.app.locals
+    const { gtin } = request.params
     const { data } = request.session
     const { __ } = response.locals
 
-    delete data.vaccines[vaccine.gtin]
+    delete data.vaccines[gtin]
 
     request.flash('success', __(`vaccine.success.delete`))
     response.redirect('/vaccines')
--- app/routes/vaccine.js
+++ app/routes/vaccine.js
@@ -3,10 +3,10 @@ import { vaccineController } from '../controllers/vaccine.js'
 
 const router = express.Router({ strict: true })
 
-router.get('/', vaccineController.list)
+router.all('/*', vaccineController.readAll)
+router.get('/', vaccineController.showAll)
 
 router.all('/:gtin*', vaccineController.read)
-
 router.get('/:gtin', vaccineController.show)
 
 router.get('/:gtin/delete', vaccineController.action('delete'))
