38033c3b14b0090ef780dc868d784f037d5d8c70
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-09-24 23:54:00 +0200

    Stub team settings for consent requests and reminders

--- app/models/organisation.js
+++ app/models/organisation.js
@@ -6,6 +6,10 @@ import { fakerEN_GB as faker } from '@faker-js/faker'
  * @property {string} [name] - Full name
  * @property {string} [email] - Email address
  * @property {string} [tel] - Phone number
+ * @property {number} [consentDelay] - Weeks before session to request consent
+ * @property {number} [reminderDelay] - Days before sending first reminder
+ * @property {number} [reminderInterval] - Days between reminders
+ * @property {number} [reminderMax] - Max number of reminders
  * @function ns - Namespace
  * @function uri - URL
  */
@@ -15,6 +19,10 @@ export class Organisation {
     this.name = options?.name
     this.email = options?.email
     this.tel = options?.tel
+    this.consentDelay = options?.consentDelay || 3
+    this.reminderDelay = options?.reminderDelay || 7
+    this.reminderInterval = options?.reminderInterval || 7
+    this.reminderMax = options?.reminderMax || 4
   }
 
   static generate() {
@@ -33,6 +41,14 @@ export class Organisation {
         .toLowerCase(),
       tel: '01### ######'.replace(/#+/g, (m) => faker.string.numeric(m.length))
     })
+  }
+
+  get formatted() {
+    return {
+      consentDelay: `${this.consentDelay} weeks before first session`,
+      reminderDelay: `${this.reminderDelay} days`,
+      reminderInterval: `${this.reminderInterval} days`
+    }
   }
 
   get ns() {
