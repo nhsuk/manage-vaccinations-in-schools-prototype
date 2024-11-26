ca6e12b5303ecdcbf67537ed29f6478435b5277e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-11-26 17:42:12 +0000

    Fix condition for not showing summary row value

--- app/globals.js
+++ app/globals.js
@@ -318,7 +318,7 @@ export default () => {
       // Allow value to be explicitly set
       let value = rows[key]?.value || formattedValue
 
-      if (typeof value !== 'undefined' || value !== 0) {
+      if (typeof value !== 'undefined' && value !== 0) {
         // Handle _unchecked checkbox value
         if (value === '_unchecked') {
           value = 'None selected'
