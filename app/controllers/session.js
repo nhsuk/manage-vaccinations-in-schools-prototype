f5c76c21d74355d8a6d465b81a5d03f7891ef66f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-01-08 22:31:17 +0000

    Change jabs to account for individual programmes

--- app/controllers/session.js
+++ app/controllers/session.js
@@ -59,7 +59,13 @@ export const sessionController = {
   activity(request, response) {
     const { activity } = request.params
     let { tab } = request.query
-    const { __, patientSessions } = response.locals
+    const { __, patientSessions, session } = response.locals
+
+    const jabTabs = [Jabs.All]
+    for (const programme of session.programmes) {
+      jabTabs.push(Jabs[programme.type])
+    }
+    jabTabs.push(Jabs.None)
 
     let tabs = []
     switch (activity) {
@@ -101,7 +107,7 @@ export const sessionController = {
         break
       case 'jabs':
         tab = tab || Jabs.All
-        tabs = [Jabs.All, Jabs.Some, Jabs.None]
+        tabs = jabTabs
         break
     }
 
