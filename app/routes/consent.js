27b2ff053f717d188064abe36fbf167da55b87bc
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-01-22 00:18:42 +0000

    Simplify consent controllers

--- app/routes/consent.js
+++ app/routes/consent.js
@@ -7,14 +7,11 @@ const router = express.Router({ strict: true })
 router.all('/*', consentController.readAll)
 router.get('/', consentController.showAll)
 
-router.get('/:uuid/match', consentController.showMatch)
+router.all('/:uuid*', consentController.read)
+router.get('/:uuid/match', consentController.match)
+router.get('/:uuid/:view', consentController.show)
 
-router.all('/:uuid/link', consentController.readLink)
-router.get('/:uuid/link', consentController.showLink)
-router.post('/:uuid/link', consentController.updateLink)
-
-router.all('/:uuid/add', consentController.readAdd)
-router.get('/:uuid/add', consentController.showAdd)
-router.post('/:uuid/add', consentController.updateAdd)
+router.post('/:uuid/link', consentController.link)
+router.post('/:uuid/add', consentController.add)
 
 export const consentRoutes = router
