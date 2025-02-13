cbaa9abdb23fe12f5a4ad860bcbeda5262496d5b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-02-13 11:24:30 +0000

    Callout important notices on dashboard

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,6 +4,7 @@ import { homeController } from '../controllers/home.js'
 
 const router = express.Router({ strict: true })
 
-router.get('/', homeController.redirect)
+router.get('/home', homeController.redirect)
+router.get('/dashboard', homeController.dashboard)
 
 export const homeRoutes = router
