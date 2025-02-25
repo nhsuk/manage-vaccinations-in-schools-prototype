3220b1ebbb2b5685641224a867c4dbff5f8870be
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-02-25 14:40:44 +0000

    Add is-sticky custom element

---
+++ app/assets/javascripts/custom-elements/is-sticky.js
@@ -0,0 +1,41 @@
+export const IsStickyComponent = class extends HTMLElement {
+  constructor() {
+    super()
+    this.stickyElementStyle = null
+    this.stickyElementTop = 0
+
+    this.determineStickyState = this.determineStickyState.bind(this)
+    this.throttledStickyState = this.throttle(this.determineStickyState, 100)
+  }
+
+  connectedCallback() {
+    this.stickyElementStyle = window.getComputedStyle(this)
+    this.stickyElementTop = parseInt(this.stickyElementStyle.top, 10)
+
+    window.addEventListener('scroll', this.throttledStickyState)
+
+    this.determineStickyState()
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('scroll', this.throttledStickyState)
+  }
+
+  determineStickyState() {
+    const currentTop = this.getBoundingClientRect().top
+    this.dataset.stuck = String(currentTop <= this.stickyElementTop)
+  }
+
+  throttle(callback, limit) {
+    let inThrottle
+    return function () {
+      const args = arguments
+      const context = this
+      if (!inThrottle) {
+        callback.apply(context, args)
+        inThrottle = true
+        setTimeout(() => (inThrottle = false), limit)
+      }
+    }
+  }
+}
