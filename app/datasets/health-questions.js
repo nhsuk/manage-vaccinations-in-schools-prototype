2cb89ae08c616bf2338224703c5a9b255e24f92a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-05-17 18:08:02 +0100

    Update health questions for flu programme
    And allow ‘Give details’ to contain optional hint text

--- app/datasets/health-questions.js
+++ app/datasets/health-questions.js
@@ -1,79 +1,96 @@
 export const healthQuestions = {
   aspirin: {
     label: 'Does the child take regular aspirin?',
-    hint: 'Also known as Salicylate therapy'
+    hint: 'Also known as Salicylate therapy',
+    detailsHint: false
   },
   allergy: {
     label: 'Does the child have any severe allergies?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   asthma: {
     label: 'Has the child been diagnosed with asthma?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   asthmaAdmitted: {
-    label: 'Has the child been admitted to intensive care for their asthma?',
-    hint: false
+    label:
+      'Has the child ever been admitted to intensive care because of their asthma?',
+    hint: false,
+    detailsHint: false
   },
   asthmaSteroids: {
-    label:
-      'Has the child taken any oral steroids for their asthma in the last 2 weeks?',
-    hint: 'Include the steroid name, dose and length of course'
+    label: 'Does the child take regular oral steroid tablets for their asthma?',
+    hint: false,
+    detailsHint: 'Include the steroid name, dose and length of course'
   },
   bleeding: {
     label:
       'Does the child have a bleeding disorder or another medical condition they receive treatment for?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   eggAllergy: {
     label:
-      'Has the child ever been admitted to intensive care due an allergic reaction to egg?',
-    hint: false
+      'Has the child ever been admitted to intensive care due to an allergic reaction to egg?',
+    hint: false,
+    detailsHint: false
   },
   immunosuppressant: {
     label: 'Does the child take any immunosuppressant medication?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   immuneSystem: {
     label:
       'Does the child have a disease or treatment that severely affects their immune system?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   householdImmuneSystem: {
     label:
       'Is anyone in the child’s household currently having treatment that severely affects their immune system?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   medicationAllergies: {
     label: 'Does the child have any allergies to medication?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   medicalConditions: {
     label:
       'Does the child have any medical conditions for which they receive treatment?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   previousReaction: {
     label:
       'Has the child ever had a severe reaction to any medicines, including vaccines?',
-    hint: false
+    hint: false,
+    detailsHint: false
   },
   recentFluVaccination: {
-    label: 'Has the child had a flu vaccination in the last 5 months?',
-    hint: false
+    label: 'Has the child had a flu vaccination in the last 3 months?',
+    hint: false,
+    detailsHint: false
   },
   recentMenAcwyVaccination: {
     label:
       'Has the child had a meningitis (MenACWY) vaccination in the last 5 years?',
-    hint: 'It’s usually given once in Year 9 or 10. Some children may have had it before travelling abroad.'
+    hint: 'It’s usually given once in Year 9 or 10. Some children may have had it before travelling abroad.',
+    detailsHint: false
   },
   recentTdIpvVaccination: {
     label:
       'Has the child had a tetanus, diphtheria and polio vaccination in the last 5 years?',
-    hint: 'Most children will not have had this vaccination since their 4-in-1 pre-school booster'
+    hint: 'Most children will not have had this vaccination since their 4-in-1 pre-school booster',
+    detailsHint: false
   },
   support: {
     label: 'Does the child need extra support during vaccination sessions?',
-    hint: 'For example, they’re autistic, or extremely anxious'
+    hint: 'For example, they’re autistic, or extremely anxious',
+    detailsHint: false
   }
 }
