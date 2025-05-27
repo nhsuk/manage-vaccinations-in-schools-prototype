2382c74a16206455d53d06feb8fbd3ad0b7a638a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-05-27 15:33:46 +0100

    Always ask for consent for injected flu vaccine

--- app/controllers/parent.js
+++ app/controllers/parent.js
@@ -131,10 +131,9 @@ export const parentController = {
       },
       [`/${session_id}/${consent_uuid}/new/address`]: {},
       ...getHealthQuestionPaths(`/${session_id}/${consent_uuid}/new/`, consent),
-      ...(consent.hasAnswersNeedingTriage &&
-        consent.decision === ReplyDecision.Given && {
-          [`/${session_id}/${consent_uuid}/new/alternative`]: {}
-        }),
+      ...(consent.decision === ReplyDecision.Given && {
+        [`/${session_id}/${consent_uuid}/new/alternative`]: {}
+      }),
       [`/${session_id}/${consent_uuid}/new/check-answers`]: {},
       [`/${session_id}/${consent_uuid}/new/confirmation`]: {},
       [`/${session_id}/${consent_uuid}/new/consultation`]: {
