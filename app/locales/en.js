60342d665e7bbad39c6cb94f89f382b1c67f5eaa
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-06-26 23:25:05 +0100

    Adjust navigation to keep all labels in view on desktop

--- app/locales/en.js
+++ app/locales/en.js
@@ -796,7 +796,7 @@ export const en = {
   },
   organisation: {
     show: {
-      label: 'Your organisation',
+      label: 'Organisation',
       title: 'Your organisation',
       description: 'Manage your organisation’s settings'
     },
@@ -2041,7 +2041,7 @@ export const en = {
       other: '[0] No imports|%s imports'
     },
     list: {
-      label: 'Import',
+      label: 'Imports',
       title: 'Import records',
       description:
         'Import child, cohort and vaccination records and see important notices'
