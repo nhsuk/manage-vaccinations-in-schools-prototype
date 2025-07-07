48c826b313a41ba8a6004866c9a1a52193444416
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-07-07 15:15:06 +0100

    Add middleware to check if within rollover period

---
+++ app/middleware/rollover.js
@@ -0,0 +1,11 @@
+import { isBetweenDates, today } from '../utils/date.js'
+
+export const rollover = (request, response, next) => {
+  response.app.locals.isRollover = isBetweenDates(
+    today(),
+    '2025-07-01',
+    '2025-08-31'
+  )
+
+  next()
+}
