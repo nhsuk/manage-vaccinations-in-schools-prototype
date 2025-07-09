a569944db4d0a220e1906453cfe1a5ca952d260f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-07-09 16:41:52 +0100

    Fix generateNotice method

--- app/generators/notice.js
+++ app/generators/notice.js
@@ -4,12 +4,12 @@ import { Notice } from '../models/notice.js'
  * Generate fake notice
  *
  * @param {import('../models/patient.js').Patient} patient - Patient
- * @param {import('../models/notice.js').NoticeType} type - Notice type
+ * @param {import('../enums.js').NoticeType} type - Notice type
  * @returns {Notice} - Notice
  */
 export function generateNotice(patient, type) {
   return new Notice({
     type,
-    patient_uuid: patient.uuid
+    patient_uuid: patient?.uuid
   })
 }
