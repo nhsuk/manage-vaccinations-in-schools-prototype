c160bc4107f8e94d5543d5a179a9b732145823a8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-07-16 21:17:55 +0100

    Reduce the number of schools used in generated data

--- app/datasets/schools.js
+++ app/datasets/schools.js
@@ -16,24 +16,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV2 2RH'
   },
-  '136126': {
-    urn: '136126',
-    name: 'Sidney Stringer Academy',
-    phase: 'Secondary',
-    addressLine1: '2 Primrose Hill Street',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV1 5LY'
-  },
-  '136963': {
-    urn: '136963',
-    name: 'Finham Park School',
-    phase: 'Secondary',
-    addressLine1: 'Green Lane',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 6EA'
-  },
   '137209': {
     urn: '137209',
     name: 'West Coventry Academy',
@@ -42,15 +24,6 @@ export default {
     addressLine2: '',
     addressLevel1: 'Coventry',
     postalCode: 'CV4 9PW'
-  },
-  '137225': {
-    urn: '137225',
-    name: 'The Westwood Academy',
-    phase: 'Secondary',
-    addressLine1: 'Mitchell Avenue',
-    addressLine2: 'Canley',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV4 8DY'
   },
   '137272': {
     urn: '137272',
@@ -79,42 +52,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV2 5BD'
   },
-  '139383': {
-    urn: '139383',
-    name: 'St Laurences CofE Primary School',
-    phase: 'Primary',
-    addressLine1: 'Old Church Road',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 7ED'
-  },
-  '139384': {
-    urn: '139384',
-    name: 'Stretton Church of England Academy',
-    phase: 'Primary',
-    addressLine1: 'Stretton Avenue',
-    addressLine2: 'Willenhall',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 3AE'
-  },
-  '139680': {
-    urn: '139680',
-    name: 'St Bartholomew’s Church of England Academy',
-    phase: 'Primary',
-    addressLine1: 'Bredon Avenue',
-    addressLine2: 'Binley',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 2LP'
-  },
-  '139681': {
-    urn: '139681',
-    name: 'Hill Farm Academy',
-    phase: 'Primary',
-    addressLine1: 'Foster Road',
-    addressLine2: 'Radford',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 3BL'
-  },
   '140248': {
     urn: '140248',
     name: 'President Kennedy School Academy',
@@ -124,33 +61,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV6 4GL'
   },
-  '140366': {
-    urn: '140366',
-    name: 'Ernesford Grange Community Academy',
-    phase: 'Secondary',
-    addressLine1: 'Princethorpe Way',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 2QD'
-  },
-  '140389': {
-    urn: '140389',
-    name: 'Radford Primary Academy',
-    phase: 'Primary',
-    addressLine1: 'Lawrence Saunders Road',
-    addressLine2: 'Radford',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 1HD'
-  },
-  '141298': {
-    urn: '141298',
-    name: 'St John’s Church of England Academy',
-    phase: 'Primary',
-    addressLine1: 'Winsford Avenue',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV5 9HZ'
-  },
   '141992': {
     urn: '141992',
     name: 'Cardinal Wiseman Catholic School',
@@ -159,33 +69,6 @@ export default {
     addressLine2: '',
     addressLevel1: 'Coventry',
     postalCode: 'CV2 2AJ'
-  },
-  '142039': {
-    urn: '142039',
-    name: 'Whittle Academy',
-    phase: 'Primary',
-    addressLine1: 'Narberth Way',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 2LH'
-  },
-  '142195': {
-    urn: '142195',
-    name: 'Corpus Christi Catholic School',
-    phase: 'Primary',
-    addressLine1: 'Langbank Avenue',
-    addressLine2: 'Ernesford Grange',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 2QP'
-  },
-  '142197': {
-    urn: '142197',
-    name: 'Good Shepherd Catholic School',
-    phase: 'Primary',
-    addressLine1: 'Spring Road',
-    addressLine2: 'Foleshill',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 7FN'
   },
   '142205': {
     urn: '142205',
@@ -204,51 +87,6 @@ export default {
     addressLine2: '',
     addressLevel1: 'Coventry',
     postalCode: 'CV2 5AT'
-  },
-  '142212': {
-    urn: '142212',
-    name: 'St John Fisher Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'St John Fisher Primary School',
-    addressLine2: 'Wyken',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 3NR'
-  },
-  '142215': {
-    urn: '142215',
-    name: 'St Patrick’s Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Deedmore Road',
-    addressLine2: 'Wood End',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 1EQ'
-  },
-  '142217': {
-    urn: '142217',
-    name: 'SS Peter and Paul Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Arkle Drive',
-    addressLine2: 'Off Woodway Lane',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 2EF'
-  },
-  '142244': {
-    urn: '142244',
-    name: 'Walsgrave Church of England Academy',
-    phase: 'Primary',
-    addressLine1: 'School House Lane',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 2BA'
-  },
-  '142245': {
-    urn: '142245',
-    name: 'Clifford Bridge Academy',
-    phase: 'Primary',
-    addressLine1: 'Coombe Park Road',
-    addressLine2: 'Binley',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 2PD'
   },
   '142339': {
     urn: '142339',
@@ -277,24 +115,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV2 3JS'
   },
-  '143177': {
-    urn: '143177',
-    name: 'Stanton Bridge Primary School',
-    phase: 'Primary',
-    addressLine1: 'Oliver Street',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 5TY'
-  },
-  '143535': {
-    urn: '143535',
-    name: 'Hearsall Community Academy',
-    phase: 'Primary',
-    addressLine1: 'Kingston Road',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV5 6LR'
-  },
   '143779': {
     urn: '143779',
     name: 'Parkgate Primary School',
@@ -303,24 +123,6 @@ export default {
     addressLine2: '',
     addressLevel1: 'Coventry',
     postalCode: 'CV6 4GF'
-  },
-  '143780': {
-    urn: '143780',
-    name: 'Keresley Grange Primary School',
-    phase: 'Primary',
-    addressLine1: 'Waste Lane',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 2EH'
-  },
-  '144064': {
-    urn: '144064',
-    name: 'Courthouse Green Primary School',
-    phase: 'Primary',
-    addressLine1: 'Sewall Highway',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 7JJ'
   },
   '145179': {
     urn: '145179',
@@ -331,87 +133,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV2 4JW'
   },
-  '145214': {
-    urn: '145214',
-    name: 'Leigh Church of England Academy',
-    phase: 'Primary',
-    addressLine1: 'Plants Hill Crescent',
-    addressLine2: 'Tile Hill',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV4 9RQ'
-  },
-  '146393': {
-    urn: '146393',
-    name: 'Richard Lee Primary School',
-    phase: 'Primary',
-    addressLine1: 'The Drive',
-    addressLine2: 'Wyken',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 5FU'
-  },
-  '146436': {
-    urn: '146436',
-    name: 'Foxford Community School',
-    phase: 'Secondary',
-    addressLine1: 'Grange Road',
-    addressLine2: 'Longford',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 6BB'
-  },
-  '147121': {
-    urn: '147121',
-    name: 'Pearl Hyde Community Primary School',
-    phase: 'Primary',
-    addressLine1: 'Dorchester Way',
-    addressLine2: 'Clifford Park',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 2NB'
-  },
-  '147341': {
-    urn: '147341',
-    name: 'St Elizabeth’s Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'St Elizabeth’s Road',
-    addressLine2: 'Foleshill',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 5BX'
-  },
-  '147342': {
-    urn: '147342',
-    name: 'St John Vianney Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Mount Nod Way',
-    addressLine2: 'Mount Nod',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV5 7GX'
-  },
-  '147343': {
-    urn: '147343',
-    name: 'St Augustine’s Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Heathcote Street',
-    addressLine2: 'Radford',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 3BL'
-  },
-  '147344': {
-    urn: '147344',
-    name: 'St Thomas More Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Knoll Drive',
-    addressLine2: 'Styvechale',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 5DE'
-  },
-  '147345': {
-    urn: '147345',
-    name: 'Bishop Ullathorne Catholic School',
-    phase: 'Secondary',
-    addressLine1: 'Leasowes Avenue',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV3 6BH'
-  },
   '147346': {
     urn: '147346',
     name: 'Cardinal Newman Catholic School',
@@ -421,15 +142,6 @@ export default {
     addressLevel1: 'Coventry',
     postalCode: 'CV6 2FR'
   },
-  '147437': {
-    urn: '147437',
-    name: 'Christ The King Catholic Primary School',
-    phase: 'Primary',
-    addressLine1: 'Scots Lane',
-    addressLine2: 'Coundon',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV6 2DJ'
-  },
   '148429': {
     urn: '148429',
     name: 'Meadow Park School',
@@ -438,41 +150,5 @@ export default {
     addressLine2: 'Whitley',
     addressLevel1: 'Coventry',
     postalCode: 'CV3 4BD'
-  },
-  '148533': {
-    urn: '148533',
-    name: 'Charter Academy',
-    phase: 'Primary',
-    addressLine1: '314 Mitchell Avenue',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV4 8DW'
-  },
-  '148603': {
-    urn: '148603',
-    name: 'Alderman’s Green Primary School',
-    phase: 'Primary',
-    addressLine1: 'Alderman’s Green Road',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 1PP'
-  },
-  '150321': {
-    urn: '150321',
-    name: 'Frederick Bird Academy',
-    phase: 'Primary',
-    addressLine1: 'Swan Lane',
-    addressLine2: '',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV2 4QQ'
-  },
-  '150405': {
-    urn: '150405',
-    name: 'Limbrick Wood Primary School',
-    phase: 'Primary',
-    addressLine1: 'Bush Close',
-    addressLine2: 'Tile Hill',
-    addressLevel1: 'Coventry',
-    postalCode: 'CV4 9QT'
   }
 }
