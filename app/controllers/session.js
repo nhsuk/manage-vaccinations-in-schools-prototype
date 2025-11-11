uk-gov-mirror/nhsuk.manage-vaccinations-in-schools-prototype

Remove Activity enum

app/controllers/session.js
@@ -3,7 +3,6 @@ import _ from 'lodash'
 
 import {
   AcademicYear,
-  Activity,
   InstructionOutcome,
   ProgrammeOutcome,
   RegistrationOutcome,
@@ -229,15 +228,8 @@ export const sessionController = {
   readPatientSessions(request, response, next) {
     const { account } = request.app.locals
     const { view } = request.params
-    let {
-      options,
-      q,
-      instruct,
-      programme_ids,
-      nextActivity,
-      vaccineCriteria,
-      yearGroup
-    } = request.query
+    let { options, q, instruct, programme_ids, vaccineCriteria, yearGroup } =
+      request.query
     const { data } = request.session
     const { session } = response.locals
 
@@ -271,13 +263,6 @@ export const sessionController = {
         : [programme_ids]
       results = results.filter((patientSession) =>
         programme_ids.includes(patientSession.programme_id)
-      )
-    }
-
-    // Filter by next activity
-    if (nextActivity && nextActivity !== 'none') {
-      results = results.filter(
-        (patientSession) => patientSession.nextActivity === nextActivity
       )
     }
 
@@ -337,8 +322,8 @@ export const sessionController = {
     // Only show patients ready to vaccinate, and that a user can vaccinate
     if (view === 'record') {
       results = results.filter(
-        ({ nextActivity, register, vaccine }) =>
-          nextActivity === Activity.Record &&
+        ({ register, report, vaccine }) =>
+          report === ProgrammeOutcome.Due &&
           register !== RegistrationOutcome.Pending &&
           account.vaccineMethods?.includes(vaccine?.method)
       )
@@ -407,7 +392,6 @@ export const sessionController = {
     delete data.instruct
     delete data.register
     delete data.report
-    delete data.nextActivity
 
     next()
   },
@@ -422,8 +406,7 @@ export const sessionController = {
       'instruct',
       'register',
       'report',
-      'vaccineCriteria',
-      'nextActivity'
+      'vaccineCriteria'
     ]) {
       const value = request.body[key]
       if (value) {
@@ -594,7 +577,7 @@ export const sessionController = {
     const { data } = request.session
 
     const patientsToInstruct = session.patientSessions
-      .filter(({ nextActivity }) => nextActivity === Activity.Record)
+      .filter(({ report }) => report === ProgrammeOutcome.Due)
       .filter(({ instruct }) => instruct === InstructionOutcome.Needed)
 
     for (const patientSession of patientsToInstruct) {

app/models/patient-session.js
@@ -2,7 +2,6 @@ import { fakerEN_GB as faker } from '@faker-js/faker'
 import filters from '@x-govuk/govuk-prototype-filters'
 
 import {
-  Activity,
   AuditEventType,
   ConsentOutcome,
   ProgrammeOutcome,
@@ -14,7 +13,6 @@ import { getDateValueDifference, getYearGroup, today } from '../utils/date.js'
 import {
   getInstructionOutcome,
   getInstructionStatus,
-  getNextActivity,
   getRegistrationOutcome,
   getReportOutcome,
   getConsentStatus,
@@ -408,22 +406,13 @@ export class PatientSession {
   }
 
   /**
-   * Get next activity
-   *
-   * @returns {Activity} Activity
-   */
-  get nextActivity() {
-    return getNextActivity(this)
-  }
-
-  /**
    * Get next activity, per programme
    *
    * @returns {Array<PatientSession>} Patient sessions per programme
    */
   get outstandingVaccinations() {
     return this.siblingPatientSessions.filter(
-      ({ nextActivity }) => nextActivity === Activity.Record
+      ({ report }) => report === ProgrammeOutcome.Due
     )
   }
 
@@ -545,7 +534,7 @@ export class PatientSession {
   /**
    * Get ready to record outcome
    *
-   * @returns {import('../enums.js').Activity} Ready to record outcome
+   * @returns {boolean} Ready to record outcome
    */
   get record() {
     return getRecordOutcome(this)

app/models/session.js
@@ -5,7 +5,6 @@ import _ from 'lodash'
 
 import {
   AcademicYear,
-  Activity,
   ConsentOutcome,
   ConsentWindow,
   InstructionOutcome,
@@ -578,7 +577,7 @@ export class Session {
       ]),
       instruct: getSessionActivityCount(this, [
         {
-          nextActivity: Activity.Record,
+          report: ProgrammeOutcome.Due,
           instruct: InstructionOutcome.Needed
         }
       ]),

app/utils/patient-session.js
@@ -1,7 +1,6 @@
 import prototypeFilters from '@x-govuk/govuk-prototype-filters'
 
 import {
-  Activity,
   ConsentOutcome,
   InstructionOutcome,
   ProgrammeOutcome,
@@ -11,42 +10,6 @@ import {
   VaccinationOutcome,
   VaccineCriteria
 } from '../enums.js'
-
-/**
- * Get next activity
- *
- * @param {import('../models/patient-session.js').PatientSession} patientSession - Patient session
- * @returns {Activity} Activity
- */
-export const getNextActivity = ({
-  consent,
-  consentGiven,
-  triage,
-  screen,
-  report
-}) => {
-  if ([ConsentOutcome.Refused, ConsentOutcome.FinalRefusal].includes(consent)) {
-    return Activity.DoNotRecord
-  }
-
-  if (!consentGiven) {
-    return Activity.Consent
-  }
-
-  if (triage === TriageOutcome.Needed) {
-    return Activity.Triage
-  }
-
-  if (screen === ScreenOutcome.DoNotVaccinate) {
-    return Activity.DoNotRecord
-  }
-
-  if (report === ProgrammeOutcome.Vaccinated) {
-    return Activity.Report
-  }
-
-  return Activity.Record
-}
 
 /**
  * Get consent status properties
@@ -368,17 +331,17 @@ export const getRegistrationOutcome = (patientSession) => {
  * Check if registration is needed prior to recording vaccination
  *
  * @param {import('../models/patient-session.js').PatientSession} patientSession - Patient session
- * @returns {RegistrationOutcome} Ready to record outcome
+ * @returns {boolean} Ready to record outcome
  */
 export const getRecordOutcome = (patientSession) => {
-  const { nextActivity, register, session } = patientSession
+  const { register, report, session } = patientSession
 
-  if (nextActivity === Activity.Record) {
+  if (report === ProgrammeOutcome.Due) {
     if (session.registration && register === RegistrationOutcome.Pending) {
-      return Activity.Register
+      return false
     }
 
-    return Activity.Record
+    return true
   }
 }
 

nhsuk/manage-vaccinations-in-schools-prototype

app/controllers/session.js
@@ -3,7 +3,6 @@ import _ from 'lodash'
 
 import {
   AcademicYear,
-  Activity,
   InstructionOutcome,
   PatientStatus,
   RegistrationOutcome,
@@ -229,15 +228,8 @@ export const sessionController = {
   readPatientSessions(request, response, next) {
     const { account } = request.app.locals
     const { view } = request.params
-    let {
-      options,
-      q,
-      instruct,
-      programme_ids,
-      nextActivity,
-      vaccineCriteria,
-      yearGroup
-    } = request.query
+    let { options, q, instruct, programme_ids, vaccineCriteria, yearGroup } =
+      request.query
     const { data } = request.session
     const { session } = response.locals
 
@@ -271,13 +263,6 @@ export const sessionController = {
         : [programme_ids]
       results = results.filter((patientSession) =>
         programme_ids.includes(patientSession.programme_id)
-      )
-    }
-
-    // Filter by next activity
-    if (nextActivity && nextActivity !== 'none') {
-      results = results.filter(
-        (patientSession) => patientSession.nextActivity === nextActivity
       )
     }
 
@@ -337,8 +322,8 @@ export const sessionController = {
     // Only show patients ready to vaccinate, and that a user can vaccinate
     if (view === 'record') {
       results = results.filter(
-        ({ nextActivity, register, vaccine }) =>
-          nextActivity === Activity.Record &&
+        ({ register, report, vaccine }) =>
+          report === PatientStatus.Due &&
           register !== RegistrationOutcome.Pending &&
           account.vaccineMethods?.includes(vaccine?.method)
       )
@@ -407,7 +392,6 @@ export const sessionController = {
     delete data.instruct
     delete data.register
     delete data.report
-    delete data.nextActivity
 
     next()
   },
@@ -422,8 +406,7 @@ export const sessionController = {
       'instruct',
       'register',
       'report',
-      'vaccineCriteria',
-      'nextActivity'
+      'vaccineCriteria'
     ]) {
       const value = request.body[key]
       if (value) {
@@ -594,7 +577,7 @@ export const sessionController = {
     const { data } = request.session
 
     const patientsToInstruct = session.patientSessions
-      .filter(({ nextActivity }) => nextActivity === Activity.Record)
+      .filter(({ report }) => report === PatientStatus.Due)
       .filter(({ instruct }) => instruct === InstructionOutcome.Needed)
 
     for (const patientSession of patientsToInstruct) {

app/models/patient-session.js
@@ -2,7 +2,6 @@ import { fakerEN_GB as faker } from '@faker-js/faker'
 import filters from '@x-govuk/govuk-prototype-filters'
 
 import {
-  Activity,
   AuditEventType,
   ConsentOutcome,
   PatientStatus,
@@ -14,7 +13,6 @@ import { getDateValueDifference, getYearGroup, today } from '../utils/date.js'
 import {
   getInstructionOutcome,
   getInstructionStatus,
-  getNextActivity,
   getRegistrationOutcome,
   getReportOutcome,
   getConsentStatus,
@@ -408,22 +406,13 @@ export class PatientSession {
   }
 
   /**
-   * Get next activity
-   *
-   * @returns {Activity} Activity
-   */
-  get nextActivity() {
-    return getNextActivity(this)
-  }
-
-  /**
    * Get next activity, per programme
    *
    * @returns {Array<PatientSession>} Patient sessions per programme
    */
   get outstandingVaccinations() {
     return this.siblingPatientSessions.filter(
-      ({ nextActivity }) => nextActivity === Activity.Record
+      ({ report }) => report === PatientStatus.Due
     )
   }
 
@@ -545,7 +534,7 @@ export class PatientSession {
   /**
    * Get ready to record outcome
    *
-   * @returns {import('../enums.js').Activity} Ready to record outcome
+   * @returns {boolean} Ready to record outcome
    */
   get record() {
     return getRecordOutcome(this)

app/models/session.js
@@ -5,7 +5,6 @@ import _ from 'lodash'
 
 import {
   AcademicYear,
-  Activity,
   ConsentOutcome,
   ConsentWindow,
   InstructionOutcome,
@@ -588,7 +587,7 @@ export class Session {
       ]),
       instruct: getSessionActivityCount(this, [
         {
-          nextActivity: Activity.Record,
+          report: PatientStatus.Due,
           instruct: InstructionOutcome.Needed
         }
       ]),

app/utils/patient-session.js
@@ -1,7 +1,6 @@
 import prototypeFilters from '@x-govuk/govuk-prototype-filters'
 
 import {
-  Activity,
   ConsentOutcome,
   InstructionOutcome,
   PatientStatus,
@@ -11,42 +10,6 @@ import {
   VaccinationOutcome,
   VaccineCriteria
 } from '../enums.js'
-
-/**
- * Get next activity
- *
- * @param {import('../models/patient-session.js').PatientSession} patientSession - Patient session
- * @returns {Activity} Activity
- */
-export const getNextActivity = ({
-  consent,
-  consentGiven,
-  triage,
-  screen,
-  report
-}) => {
-  if ([ConsentOutcome.Refused, ConsentOutcome.FinalRefusal].includes(consent)) {
-    return Activity.DoNotRecord
-  }
-
-  if (!consentGiven) {
-    return Activity.Consent
-  }
-
-  if (triage === TriageOutcome.Needed) {
-    return Activity.Triage
-  }
-
-  if (screen === ScreenOutcome.DoNotVaccinate) {
-    return Activity.DoNotRecord
-  }
-
-  if (report === PatientStatus.Vaccinated) {
-    return Activity.Report
-  }
-
-  return Activity.Record
-}
 
 /**
  * Get consent status properties
@@ -368,17 +331,17 @@ export const getRegistrationOutcome = (patientSession) => {
  * Check if registration is needed prior to recording vaccination
  *
  * @param {import('../models/patient-session.js').PatientSession} patientSession - Patient session
- * @returns {RegistrationOutcome} Ready to record outcome
+ * @returns {boolean} Ready to record outcome
  */
 export const getRecordOutcome = (patientSession) => {
-  const { nextActivity, register, session } = patientSession
+  const { register, report, session } = patientSession
 
-  if (nextActivity === Activity.Record) {
+  if (report === PatientStatus.Due) {
     if (session.registration && register === RegistrationOutcome.Pending) {
-      return Activity.Register
+      return false
     }
 
-    return Activity.Record
+    return true
   }
 }
 