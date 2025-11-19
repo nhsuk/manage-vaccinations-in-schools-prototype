3ef720c3b34bbd35b6966fab77eb7c541f136b85
Mike Thompson <mike@artifactual.co.uk>
2025-11-19 17:14:14 +0000

    Add programme and year to report download form

--- app/controllers/download.js
+++ app/controllers/download.js
@@ -1,21 +1,36 @@
+import programmesData from '../datasets/programmes.js'
+import { AcademicYear, DownloadFormat, ProgrammeType } from '../enums.js'
 import { Download } from '../models/download.js'
 import { Organisation } from '../models/organisation.js'
 import { Programme } from '../models/programme.js'
 
 export const downloadController = {
-  redirect(request, response) {
-    const { programme_id } = request.params
-
-    response.redirect(`/reports/${programme_id}`)
-  },
-
   form(request, response) {
-    const { programme_id } = request.params
     const { data } = request.session
 
-    const programme = Programme.findOne(programme_id, data)
+    const academicYearKeys = Object.keys(AcademicYear)
+    const mostRecentYear = academicYearKeys[academicYearKeys.length - 1]
 
-    response.locals.programme = programme
+    response.locals.academicYearItems = Object.entries(AcademicYear).map(
+      ([value, text]) => ({
+        text,
+        value,
+        checked: value === mostRecentYear
+      })
+    )
+
+    response.locals.programmeTypeItems = Object.entries(ProgrammeType).map(
+      ([value, text]) => ({
+        text,
+        value,
+        checked: value === 'Flu'
+      })
+    )
+
+    response.locals.download = {
+      format: DownloadFormat.CSV
+    }
+
     response.locals.organisationItems = Organisation.findAll(data).map(
       (organisation) => ({
         text: organisation.name,
@@ -23,21 +38,21 @@ export const downloadController = {
       })
     )
     response.locals.paths = {
-      back: `/reports/${programme_id}`,
-      next: `/reports/${programme_id}/download/new`
+      back: '/reports',
+      next: '/reports/download/new'
     }
 
     response.render('download/form')
   },
 
   create(request, response) {
-    const { programme_id } = request.params
     const { account } = request.app.locals
     const { data } = request.session
 
+    const { type } = request.body.download
+    const programme_id = programmesData[type].id
     const programme = Programme.findOne(programme_id, data)
 
-    // Create download record
     const createdDownload = Download.create(
       {
         ...request.body.download,
@@ -48,7 +63,6 @@ export const downloadController = {
       data
     )
 
-    // Create Download instance with full context to access vaccinations
     const download = new Download(createdDownload, data)
 
     // Generate and return file
