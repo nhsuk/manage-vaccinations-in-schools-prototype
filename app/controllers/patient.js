uk-gov-mirror/nhsuk.manage-vaccinations-in-schools-prototype

Add ability to find children without contact details

app/controllers/patient.js
@@ -46,7 +46,12 @@ export const patientController = {
     }
 
     // Filter by display option
-    for (const option of ['archived', 'hasMissingNhsNumber', 'post16']) {
+    for (const option of [
+      'archived',
+      'hasMissingNhsNumber',
+      'hasNoContactDetails',
+      'post16'
+    ]) {
       if (options?.includes(option)) {
         patients = patients.filter((patient) => patient[option])
       }

app/controllers/session.js
@@ -343,7 +343,12 @@ export const sessionController = {
     }
 
     // Filter by display option
-    for (const option of ['archived', 'hasMissingNhsNumber', 'post16']) {
+    for (const option of [
+      'archived',
+      'hasMissingNhsNumber',
+      'hasNoContactDetails',
+      'post16'
+    ]) {
       if (options?.includes(option)) {
         results = results.filter(({ patient }) => patient[option])
       }

nhsuk/manage-vaccinations-in-schools-prototype

app/controllers/patient.js
@@ -145,7 +145,12 @@ export const patientController = {
     }
 
     // Filter by display option
-    for (const key of ['archived', 'hasMissingNhsNumber', 'post16']) {
+    for (const key of [
+      'archived',
+      'hasMissingNhsNumber',
+      'hasNoContactDetails',
+      'post16'
+    ]) {
       if (option?.includes(key)) {
         results = results.filter((patient) => patient[key])
       }

app/controllers/session.js
@@ -326,7 +326,12 @@ export const sessionController = {
     }
 
     // Filter patient by display option
-    for (const key of ['archived', 'hasMissingNhsNumber', 'post16']) {
+    for (const key of [
+      'archived',
+      'hasMissingNhsNumber',
+      'hasNoContactDetails',
+      'post16'
+    ]) {
       if (option?.includes(key)) {
         results = results.filter(({ patient }) => patient[key])
       }