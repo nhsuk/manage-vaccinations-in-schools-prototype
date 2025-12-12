fafe879bb342df20b91c77242eafd68f5b405436
Frankie Roberto <frankie@frankieroberto.com>
2025-12-12 22:58:20 +0000

    Switch app.js to use ES modules

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const { join } = require('node:path')
-const { format: urlFormat } = require('node:url')
-
 // External dependencies
-const express = require('express')
-const nunjucks = require('nunjucks')
+import express from 'express'
+import nunjucks from 'nunjucks'
+
+import { join } from 'node:path'
+
+import NHSPrototypeKit from 'nhsuk-prototype-kit'
+
 
 // Local dependencies
-const routes = require('./app/routes')
-const filters = require('./app/filters')
-const sessionDataDefaults = require('./app/data')
-
-const NHSPrototypeKit = require('nhsuk-prototype-kit')
+import routes from './app/routes.js'
+import filters from './app/filters.js'
+import sessionDataDefaults from './app/data.js'
 
 // Set configuration variables
 const port = parseInt(process.env.PORT, 10) || 2000
@@ -20,11 +20,12 @@ const app = express()
 
 // Nunjucks configuration for application
 const appViews = [
-  join(__dirname, 'app/views/'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/components'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/macros'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist')
+  join(import.meta.dirname, 'app/views/'),
+  join(import.meta.dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/components'),
+  join(import.meta.dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/macros'),
+  join(import.meta.dirname, 'node_modules/nhsuk-frontend/dist/nhsuk'),
+  join(import.meta.dirname, 'node_modules/nhsuk-frontend/dist'),
+  join(import.meta.dirname, 'node_modules/nhsuk-decorated-components')
 ]
 
 const nunjucksConfig = {
