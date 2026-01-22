30b4fe9e3cebf13702112d6b49d9b32aa64d236f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-01-22 13:45:05 +0000

    Allow recording of a previous vaccination from patient session page (outside of an in-progress session)

--- app/locales/en.js
+++ app/locales/en.js
@@ -1256,6 +1256,10 @@ export const en = {
       titleWithMethod:
         'Record {{session.programmeNames.sentenceCase}} vaccination with {{method}}'
     },
+    recordPrevious: {
+      title:
+        'Record a previous {{session.programmeNames.sentenceCase}} vaccination'
+    },
     registration: {
       label: 'Is {{patient.fullName}} attending today’s session?',
       title: 'Update attendance',
